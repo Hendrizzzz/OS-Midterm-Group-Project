1c1918c5454f282d07d172d0d42c52467473b80a
ALDYN ZANDREX G. LAWAGAN <2240895@slu.edu.ph>
2024-10-22 23:54:02 +0800

    display champions

--- scripts/champions.sh
+++ scripts/champions.sh
@@ -1,12 +1,13 @@
-#!/bin/bash/
+#!/bin/bash
 
 past_champions (){
 
+file_content=$(cat /home/ubuntu/OS-Midterm-Group-Project/databases/champions.csv)
 echo "===================================================================="
 echo "  			  CHAMPIONS                               "
 echo "===================================================================="
 
-awk -F, '{ printf "%-20s %-10s %-10s %-10s\n", $1, $2, $3, $4 }' /OS-Midterm-Group-Project/databases/champions.csv | column -s, -t 
+echo "$file_content" | awk -F, '{ printf "%-10s %-25s %-30s %-10s\n", $1, $2, $3, $4 }' 
 echo "===================================================================="
 
 }
