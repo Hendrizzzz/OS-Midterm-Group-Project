e0584c1e94c44d950c111c2d2ffc63fc1bc3928e
ALDYN ZANDREX G LAWAGAN <2240895@slu.edu.ph>
2024-10-27 19:56:28 +0800

    Total redo of delete feature

--- scripts/delete_player.sh
+++ scripts/delete_player.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
 delete_player () {
-    file="../databases/players.csv"
-    
-    # Check if the file exists
-    if [[ ! -f "$file" ]]; then
-        echo "File not found: $file"
-        return 1
-    fi
+	 name_to_delete="$1"
 
-    grep "2024" "$file" | awk -F, -v player="$1" '$1 != player' > temp
-    if [[ $? -eq 0 ]]; then  
-        mv temp "$file"
-        echo "Player '$1' deleted successfully."
-    else
-        echo "Error filtering players."
-        rm temp 
-    fi
+	#path to the database
+	file="../databases/players.csv"
+
+	# Find the line number the player name to be deleted
+	line_number=$(grep -n "$name_to_delete" "$file" | tail -n 1 | cut -d: -f1)
+	# Check if the name was found
+	if [ -n "$line_number" ]; then
+  	#Delete the line with the last occurrence of the name or also deletes the only occurence of the name
+  	sed -i "${line_number}d" "$file"
+  	echo "Deleted '$name_to_delete' at line $line_number."
+	else
+  	echo "Name '$name_to_delete' not found in the database."
+	fi
 }
 
-echo "Enter the name of the player you wish to delete:"
+echo "Enter the player name that you want to delete"
 read a
 
 delete_player "$a"
